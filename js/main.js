fa5f4e59dd35934b0cfe04f028fe1f64d526b053
gonzalovillalon <105450730+gonzalovillalon@users.noreply.github.com>
2023-11-21 20:01:32 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,39 +1,40 @@
 alert("Bienvenido a Dejavu Vinos y Destilados" )
 alert("A continuaciòn ingrese sus datos personales")
-let usuarios=[]
-
-let usuario1={
-  nombre: prompt("Ingrese su Nombre"),
-  apellido: prompt("Ingrese su apellido"),
-  edad: parseInt(prompt("Ingrese su edad")),
+const Usuario=function(nombre,apellido,edad){
+  this.nombre=nombre,
+  this.apellido=apellido,
+  this.edad=edad;
 }
-alert("Invitando a dos amigos accedes a maravillosas promos!!")
+
+function PrimerUsuario() {
+  let usuario1=new Usuario( prompt("Ingrese su Nombre").toLowerCase(),prompt("Ingrese su apellido").toLowerCase(),parseInt(prompt("Ingrese su edad")))
+  if (usuario1.nombre ==null || usuario1.apellido ==null || isNaN(usuario1.edad)) {
+    alert("Te has olvidado de ingresar tus datos,reinicia la pagina para volver a ingresar los datos")}
+    
+    else{
+      alert("Invitando a dos amigos accedes a maravillosas promos!!")
+      Confirmacion()
+    }
+  }
+  PrimerUsuario() 
+  
+  
 
 function Confirmacion() {
+  let usuarios=[]
   let valor= confirm("¿Tienes dos amigos para ingresar a las promos ?");
-  if (valor=== true) {
-    alert("A continuaciòn ingrese los datos de tus amigos invitados")
-    
-      let usuario2={
-        nombre: prompt("Ingrese su Nombre"),
-        apellido: prompt("Ingrese su apellido"),
-        edad: parseInt(prompt("Ingrese su edad")),
-      }
-      let usuario3={
-        nombre: prompt("Ingrese su Nombre"),
-        apellido: prompt("Ingrese su apellido"),
-        edad: parseInt(prompt("Ingrese su edad")),
-      }
-      usuarios.push(usuario1,usuario2,usuario3)
-  }else{
-    alert("No podras acceder a las promos !")
-  }
+    if (valor=== true) {
+      alert("A continuaciòn ingrese los datos de tus amigos invitados")
+      let usuario2=new Usuario( prompt("Ingrese su Nombre").toLowerCase(),prompt("Ingrese su apellido").toLowerCase(),parseInt(prompt("Ingrese su edad")))
+      let usuario3=new Usuario( prompt("Ingrese su Nombre").toLowerCase(),prompt("Ingrese su apellido").toLowerCase(),parseInt(prompt("Ingrese su edad")))
+     usuarios.push(usuario2,usuario3);
+     let acceso= usuarios.filter(usuario=> (usuario.edad >=18 ) );
+     
+   if (acceso.length>1) {
+      alert("Acceso Confirmado")
+      
+    }else{
+      alert("No cumples con los requisitos de edad de tus invitados,vuelve a intentarlo para poder accesder a las promos")
+    }
 }
-Confirmacion()
-
-function Registro() {
-  let acceso= usuarios.filter(usuario=> (usuario.edad >=18 ) );
-  console.table(acceso)
 }
-Registro() 
-
