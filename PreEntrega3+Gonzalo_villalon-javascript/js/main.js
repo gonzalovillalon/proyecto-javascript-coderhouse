bc9da70f50ef2ac71178856b42feeaec571a3d38
gonzalovillalon <105450730+gonzalovillalon@users.noreply.github.com>
2023-12-06 10:03:25 -0300

    Add files via upload

--- PreEntrega3+Gonzalo_villalon-javascript/js/main.js
+++ PreEntrega3+Gonzalo_villalon-javascript/js/main.js
@@ -1,76 +1,3 @@
-
-//CREACION DE OBJETO DE LAS CARTAS 
-const Cartas = function (nombre, imagen, descripcion, precio) {
-  this.nombre = nombre;
-  this.imagen = imagen;
-  this.descripcion = descripcion;
-  this.precio = precio;
-
-}
-const whiskys = [
-  new Cartas("Jim Beam", "/assets/image/foto.png.jpg", " Jim Beam es una marca norteamericana de whisky de Bourbon producida en Clermont, Kentucky. Fue una de las marcas más vendidas durante el año 2008.", 48.000),
-  new Cartas("Blue Label", "/assets/image/bluelabel.jpg", ">Johnnie Walker Blue Label es una obra maestra inigualable. Es una mezcla exquisita hecha con algunos de los whiskies más raros y excepcionales de Escocia.", 100.000),
-  new Cartas("Johnnier Walker", "/assets/image/johnnierwalker.jpg", "Johnnie Walker 18 Años es una combinación suave y completa de hasta 18 whiskies diferentes de toda Escocia", 120.000)
-]
-
-console.log(whiskys)
-
-//iMPRIMIR CARTAS
-const contenedor = document.getElementById("cards")
-if (contenedor) { 
-  function ImpresionYCarrito(){
-
-    whiskys.forEach((whiskys=> {
-      let cardd = document.createElement("div")
-          cardd.innerHTML = `
-            
-            <div class="card" id="contenedorCards"style="width: 18rem;">
-              <img src="${whiskys.imagen}" class="card-img-top" alt="...">
-              <div class="card-body">
-                <h5 id="nombreProducto"class="card-title">${whiskys.nombre}</h5>
-                <p class="card-text">${whiskys.descripcion}</p>
-                <div>
-                <p id="precioProducto"class="card-text">${whiskys.precio}</p>
-                <div class="col-12">
-                <button type="submit" id="añadirCarrito"class="btn btn-primary">añadir al carrito</button>
-              </div>
-                
-                <input type="number"placeholder="Cantidad" id="cantidad" name="tentacles"  />
-      
-                </div>
-              </div>
-            </div>`
-      
-          contenedor.appendChild(cardd)
-          let botonAñadirCarrito=document.getElementById("añadirCarrito")
-          
-          botonAñadirCarrito.addEventListener("click",e=>{
-            let cantidad=document.getElementById("cantidad")
-            console.log([cantidad])
-          
-            e.preventDefault()
-          
-              let objetoNuevo={
-                nombre:whiskys.nombre,
-                precio:whiskys.precio,
-              }
-              objetoNuevo.cantidad= cantidad
-              
-            console.log(objetoNuevo)
-            
-          });
-        
-          
-    })
-        )
-      }
-     
-
-      ImpresionYCarrito()
-  }
-  
-
-
 let nombre = document.getElementById("nombre")
 let apellido = document.getElementById("apellido");
 let edad = document.getElementById("edad");
@@ -80,6 +7,10 @@ let direccion = document.getElementById("direccion");
 let tipoDireccion = document.getElementById("tipoDireccion");
 let ciudad = document.getElementById("ciudad");
 let telefono = document.getElementById("telefono");
+let usuario = document.getElementById("nombreUsuario");
+let enviar = document.getElementById("enviar");
+let formulario = document.getElementById("formulario");
+const contenedor = document.getElementById("cards")
 
 
 //objeto contructor
@@ -94,20 +25,72 @@ function Ingreso(nombre, apellido, edad, email, direccion, tipoDireccion, ciudad
   this.telefono = telefono
 }
 
+//CREACION DE OBJETO DE LAS CARTAS 
+const Cartas = function (nombre, imagen, descripcion, precio,id) {
+  this.nombre = nombre;
+  this.imagen = imagen;
+  this.descripcion = descripcion;
+  this.precio = precio;
+  this.id = id;
+
+}
+const whiskys = [
+  new Cartas("Jim Beam", "/assets/image/foto.png.jpg", " Jim Beam es una marca norteamericana de whisky de Bourbon producida en Clermont, Kentucky. Fue una de las marcas más vendidas durante el año 2008.","$ 48.000",1),
+  new Cartas("Blue Label", "/assets/image/bluelabel.jpg", ">Johnnie Walker Blue Label es una obra maestra inigualable. Es una mezcla exquisita hecha con algunos de los whiskies más raros y excepcionales de Escocia.","$ 100.000",2),
+  new Cartas("Johnnier Walker", "/assets/image/johnnierwalker.jpg", "Johnnie Walker 18 Años es una combinación suave y completa de hasta 18 whiskies diferentes de toda Escocia", "$ 120.000",3)
+]
+//iMPRIMIR CARTAS
+
+
+function Impresion() {
+  whiskys.forEach((datos => {
+    let cardd = document.createElement("div")
+    cardd.innerHTML = `
+             <div class="card" id="contenedorCards"style="width: 18rem;">
+              <img src="${datos.imagen}" class="card-img-top" alt="...">
+              <div class="card-body">
+                <h5 id="nombreProducto"class="card-title">${datos.nombre}</h5>
+                <p class="card-text">${datos.descripcion}</p>
+                <div>
+                <p id="precioProducto"class="card-text">${datos.precio}</p>
+                <div class="col-12">
+                <button type="submit" id="añadirCarrito"class="btn btn-primary">añadir al carrito</button>
+              </div>
+                <input type="number"placeholder="Cantidad" id="cantidad" name="tentacles"  />
+      
+                </div>
+              </div>
+            </div>`
+
+    contenedor.appendChild(cardd)
+  })
+  )
+}
+
+function añadirCarrito() {
+  const botonAñadirCarrito = document.getElementById("añadirCarrito")
+  console.log(botonAñadirCarrito)
+botonAñadirCarrito.addEventListener("click",( e => {
+  e.preventDefault;
+  whiskys.find((datos=>{
+    let objetoNuevo={
+      nombre:datos.nombre,
+      precio:datos.precio,
+      id:datos.id,
+    }
+    let cantidad=document.getElementById('cantidad')
+    objetoNuevo.cantidad=cantidad
+    console.log(objetoNuevo)
+  }))
+
+  
+})
+)}
 
 
 
-//nombre de usuario ingresado
-let usuario = document.getElementById("nombreUsuario");
-
-
-//Boton
-let enviar = document.getElementById("enviar");
-
-let formulario = document.getElementById("formulario");
-
-if (formulario) {
-  enviar.addEventListener("click", e => {
+function Usuario() {
+  enviar.addEventListener("click",( e => {
     e.preventDefault();
     let valorNombre = nombre.value;
     let valorApellido = apellido.value;
@@ -125,7 +108,7 @@ if (formulario) {
     localStorage.setItem("usuarioIngresado", JSON.stringify(datos));
 
   })
-}
+)}
 
 window.onload = function () {
   let valorGuardado = localStorage.getItem("usuarioIngresado")
@@ -136,14 +119,23 @@ window.onload = function () {
   }
 }
 
-
-//Funcion cerrar sesion (eliminacion de LocalStorage)
 let cerrarSesion = document.getElementById("cerra-sesion")
 cerrarSesion.addEventListener("click", Eliminar)
 function Eliminar() {
   localStorage.removeItem("usuarioIngresado")
   location.href = "/index.html"
 }
+
+if (contenedor) {
+  Impresion();
+ 
+}
+if (formulario) {
+  Usuario()
+}
+
+añadirCarrito() 
+
 
 
 
